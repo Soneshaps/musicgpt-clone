81fec583edd5d7c0889e3e81151bbbb4b7e615ad
Soneshaps <sonesh.aps@gmail.com>
2025-08-16 01:03:08 +0545

    styles: fix tools transition on dropdown select

--- frontend/src/components/common/dropdown/dropdown.tsx
+++ frontend/src/components/common/dropdown/dropdown.tsx
@@ -70,7 +70,7 @@ const Dropdown: FunctionComponent<DropdownProps> = ({
 
       <div
         className={twclsx(
-          "absolute top-full z-50 transform overflow-hidden rounded-2xl border border-neutral-base bg-neutral-black/85 p-1.5 shadow-lg transition-all duration-300 ease-in-out max-sm:w-full",
+          "absolute top-full z-50 transform overflow-hidden rounded-2xl border border-neutral-base bg-[rgba(29,33,37,1)] backdrop-blur-[50px] p-1.5 shadow-lg transition-all duration-300 ease-in-out max-sm:w-full",
           {
             "right-1/2 w-full translate-x-1/2": align === "center",
             "left-0": align === "left",
--- frontend/src/components/prompt-box.tsx
+++ frontend/src/components/prompt-box.tsx
@@ -3,6 +3,7 @@ import { FormActions } from "./tools/form-actions";
 import { TextToSpeechTool } from "./tools/text-to-speech-tool";
 import { ToolType } from "./music-gpt-interface";
 import { useState } from "react";
+import { twclsx } from "@/utils/twclsx";
 
 const PromptBox = ({
   selectedTool,
@@ -15,18 +16,34 @@ const PromptBox = ({
   return (
     <div className="relative z-10 w-full rounded-[27px] bg-[#272A2E] shadow-lg backdrop-blur-sm transition duration-200">
       <form onSubmit={() => {}} className="overflow-hidden pb-4">
-        <div className="transition-all duration-500 ease-in-out px-5 pt-5">
-          {selectedTool === ToolType.TEXT_TO_SPEECH ? (
-            <>
+        <div className="px-5 pt-5">
+          <div
+            className={twclsx("transition-all duration-500 ease-in-out ", {
+              "translate-x-0 opacity-100":
+                selectedTool === ToolType.TEXT_TO_SPEECH,
+              "pointer-events-none absolute inset-0 -translate-x-full opacity-0":
+                selectedTool !== ToolType.TEXT_TO_SPEECH,
+            })}
+          >
+            {selectedTool === ToolType.TEXT_TO_SPEECH && (
               <TextToSpeechTool
                 prompt={"asd"}
                 onPromptChange={() => {}}
                 selectedVoice={null}
                 onVoiceSelect={() => {}}
               />
-            </>
-          ) : (
-            <>
+            )}
+          </div>
+
+          <div
+            className={twclsx("transition-all duration-500 ease-in-out", {
+              "translate-x-0 opacity-100":
+                selectedTool == ToolType.CREATE_ANYTHING,
+              "pointer-events-none absolute inset-0 translate-x-full opacity-0":
+                selectedTool !== ToolType.CREATE_ANYTHING,
+            })}
+          >
+            {selectedTool === ToolType.CREATE_ANYTHING && (
               <CreateAnythingTool
                 prompt={"asd"}
                 lyrics={"asd"}
@@ -34,8 +51,8 @@ const PromptBox = ({
                 onPromptChange={() => {}}
                 onLyricsChange={() => {}}
               />
-            </>
-          )}
+            )}
+          </div>
 
           <FormActions
             activeMode={activeMode}
